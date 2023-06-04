721ec448a5ab982b38fa8b09a70296e3f33a7431
Davidson Ratis <davidsonmakarios@gmail.com>
2023-06-04 20:36:11 -0300

    created - constants config

---
+++ src/config/constants.ts
@@ -0,0 +1,5 @@
+export const errors = {
+	NOT_FOUND: 'Not Found',
+	INT_SERV_ERR: 'Internal Server Error',
+	INT_SERV_ERR_MSG: 'Server is not responding. If the problem persists, contact your system administrator'
+} as const;
